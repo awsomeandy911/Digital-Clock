58bd32ca44bd29e555fbd044f1079662eeaa5d6a
Andy Le <47935760+awsomeandy911@users.noreply.github.com>
2023-05-19 14:29:54 -0500

    Added weekday, month, year

--- DigitalClock.py
+++ DigitalClock.py
@@ -5,17 +5,21 @@ window = ui.Tk()
 
 # Updates clock to current time
 def updateClock():
-    hours = time.strftime("%I")
-    minutes = time.strftime("%M")
-    seconds = time.strftime("%S")
+    weekDay = time.strftime("%A")
+    month = time.strftime("%B")
+    day = time.strftime("%d")
+    year = time.strftime("%Y")
+    hour = time.strftime("%I")
+    minute = time.strftime("%M")
+    second = time.strftime("%S")
     AM_PM = time.strftime("%p")
-    clockText = hours + ":" + minutes + ":" + seconds + " " + AM_PM
+    clockText = weekDay + ", " + month + " " + day + ", " + year + " " + hour + ":" + minute + ":" + second + " " + AM_PM
     digitalclockLabel.config(text = clockText)
-    # after label allows to keep updating clock by the second (1000 miliseconds)
+    # after label allows to keep updating clock by the second (1000 milisecond)
     digitalclockLabel.after(1000, updateClock)
 
 # Label to hold the current time for clock
-digitalclockLabel = ui.Label(window, text = "00:00:00", font = "Helvetica 100 bold")
+digitalclockLabel = ui.Label(window, text = "00:00:00", font = "Helvetica 69 bold")
 digitalclockLabel.pack()
 
 # use updateClock function
